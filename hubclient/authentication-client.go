8ed24aa432e9fac0a9ca1a29f8f0c815de0e7640
Claire Keum <ckeum@blackducksoftware.com>
2018-08-30 11:29:33 -0400

    return rsa.PublicKey object instead of string

--- hubclient/authentication-client.go
+++ hubclient/authentication-client.go
@@ -1,15 +1,17 @@
 package hubclient
 
 import (
+	"crypto/rsa"
 	"fmt"
 
 	"github.com/blackducksoftware/hub-client-go/hubapi"
+	jwt "github.com/dgrijalva/jwt-go"
 	log "github.com/sirupsen/logrus"
 )
 
 // Public Key is retrieved with global setting API in Hub.
 
-func (c *Client) GetPublicKey() (*hubapi.GlobalSetting, error) {
+func (c *Client) GetPublicKey() (*rsa.PublicKey, error) {
 	var publicKey hubapi.GlobalSetting
 	publicKeyURL := fmt.Sprintf("%s/api/public-key", c.baseURL)
 	err := c.HttpGetJSON(publicKeyURL, &publicKey, 200)
@@ -18,5 +20,20 @@ func (c *Client) GetPublicKey() (*hubapi.GlobalSetting, error) {
 		return nil, err
 	}
 
-	return &publicKey, nil
+	key, err := LoadKeyFromString(publicKey.Value)
+	if err != nil {
+		log.Errorf("Could not load key from the public key string", err)
+		return nil, err
+	}
+	return key, nil
+}
+
+func LoadKeyFromString(publicKeyString string) (*rsa.PublicKey, error) {
+	key := "-----BEGIN PUBLIC KEY-----\n" + publicKeyString + "\n-----END PUBLIC KEY-----"
+	rsaKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(key))
+	if err != nil {
+		log.Errorf("Cannot parse the public key string into a key", err)
+		return nil, err
+	}
+	return rsaKey, nil
 }
