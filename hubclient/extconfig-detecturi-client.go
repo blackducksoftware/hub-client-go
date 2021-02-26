b64106040bff17d1ba55b01b78c318adf72593aa
Tarun Sethi <tarun@codiscope.com>
2021-02-26 12:19:29 -0800

    Fixed copyright

--- hubclient/extconfig-detecturi-client.go
+++ hubclient/extconfig-detecturi-client.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Synopsys, Inc.
+// Copyright 2021 Synopsys, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
