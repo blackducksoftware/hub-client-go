400cacbe55f5d5fec0d07e5b33268cbc28a0394d
Jeremy Dartigalongue <jeremy.dartigalongue@outlook.com>
2023-02-28 13:39:39 -0500

    add back typo in Medium field

--- hubapi/rapid-scans-api.go
+++ hubapi/rapid-scans-api.go
@@ -54,7 +54,7 @@ type ComponentLicense struct {
 type Risk struct {
 	Critical int `json:"critical"`
 	High     int `json:"high"`
-	Medium   int `json:"medium"`
+	Medimum  int `json:"medium"`
 	Low      int `json:"low"`
 	Unscored int `json:"unscored"`
 }
